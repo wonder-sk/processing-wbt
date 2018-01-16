84b51c94ddd4f17c278b3ed0d85121342abead05
Alexander Bruy <alexander.bruy@gmail.com>
2018-01-16 15:12:43 +0200

    infrastructure for localizetion (work in progress)

---
+++ i18n/processing_whitebox_uk.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0">
+<context>
+    <name>WhiteboxProvider</name>
+    <message>
+        <location filename="../whiteboxProvider.py" line="64"/>
+        <source>Activate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../whiteboxProvider.py" line="68"/>
+        <source>Whitebox Tools executable</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../whiteboxProvider.py" line="73"/>
+        <source>Log commands output</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../whiteboxProvider.py" line="115"/>
+        <source>Could not load WhiteBox Tools algorithm from file: {}</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../whiteboxProvider.py" line="118"/>
+        <source>Processing</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../whiteboxProvider.py" line="118"/>
+        <source>Could not load WhiteBox Tools algorithm from file: {}
+{}</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ processing_whitebox.pro
@@ -0,0 +1,7 @@
+SOURCES = __init__.py \
+          whiteboxProviderPlugin.py \
+          whiteboxProvider.py \
+          whiteboxAlgorithm.py \
+          whiteboxUtils.py
+
+TRANSLATIONS = i18n/processing_whitebox_uk.ts
